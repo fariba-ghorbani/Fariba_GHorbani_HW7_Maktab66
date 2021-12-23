1b2b299aaf9186b6c58d854e865a8b0a45958098
Fariba <fariba.ghorbani2127@gmail.com>
2021-12-23 09:27:08 +0330

    HW7 is modified

--- 1/1.js
+++ 1/1.js
@@ -4,12 +4,6 @@ function ClassConstructor(name, unit, capacity) {
     this.classCapacity = capacity;
 }
 
-const softEngineering = new ClassConstructor('softEngineering', 3, 50)
-const network = new ClassConstructor('network', 3, 30)
-
-softEngineering.book = "Clean Code"
-network.project = true
-
 function typeClassProps (obj) {
     let temp = Object.getOwnPropertyNames(obj)
     let string = ""
@@ -18,6 +12,15 @@ function typeClassProps (obj) {
     }
     return "{"+string + temp[temp.length-1] + ":" + obj[temp[temp.length-1]] + "}"
 }
+
+const softEngineering = new ClassConstructor('softEngineering', 3, 50)
+const network = new ClassConstructor('network', 3, 30)
+
+console.log(typeClassProps(softEngineering))
+console.log(typeClassProps(network))
+
+softEngineering.book = "Clean Code"
+network.project = true
 
 console.log(typeClassProps(softEngineering))
 console.log(typeClassProps(network))
